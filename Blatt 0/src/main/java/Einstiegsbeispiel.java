562362f21f0544d5039d1309cced0ca0120121db
TwisT8642 <baisan.andreas@gmail.com>
2021-10-19 12:17:02 +0200

    Class Einstiegsbeispiel and EinstiegsbeispielTest fertig

---
+++ Blatt 0/src/main/java/Einstiegsbeispiel.java
@@ -0,0 +1,38 @@
+public class Einstiegsbeispiel {
+
+    public int [] a;
+    public int [] b;
+
+    public Einstiegsbeispiel(int[] a, int[] b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int[] getA() {
+        return a;
+    }
+
+    public void setA(int[] a) {
+        this.a = a;
+    }
+
+    public int[] getB() {
+        return b;
+    }
+
+    public void setB(int[] b) {
+        this.b = b;
+    }
+
+    public int getInnerProduct () {
+        int c = a.length-1;
+        int skpro = 0;
+        if (c > 0) {
+            skpro += a[c] * b[c];
+            c -= 1;
+        }
+        System.out.println(skpro);
+        return skpro;
+    }
+
+}
---
+++ Blatt 0/src/test/java/EinstiegsbeispielTest.java
@@ -0,0 +1,13 @@
+public class EinstiegsbeispielTest {
+    int []f = {3,5,7};
+    int []g = {2,4,6};
+
+    Einstiegsbeispiel test = new Einstiegsbeispiel(f, g);
+
+    int skapro = test.getInnerProduct();
+
+
+
+
+    }
+
