0bf850e7c4db50e26301e4e420389478e22e0df5
TwisT8642 <baisan.andreas@gmail.com>
2022-01-10 16:45:32 +0100

    ÜB4 angefangen

--- Blatt4/Angabe/projekt/src/test/java/at/aau/ue4/bsp1/BaseTest.java
+++ Blatt4/Angabe/projekt/src/test/java/at/aau/ue4/bsp1/BaseTest.java
@@ -3,8 +3,28 @@ package at.aau.ue4.bsp1;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
 public class BaseTest {
 
+    private RingBuffer<Integer> ringBuffer = new RingBuffer(5);
+    private Iterator ringBufferIterator = ringBuffer.iterator();
+
     @Test
-    public void test(){}
+    public void ifIsEmpty_ReturnTrue(){
+        assertTrue(ringBuffer.isEmpty());
+    }
+    @Test
+    public void ifRingBufferCapacityIs0_thenReturn0(){
+        assertEquals(0, ringBuffer.size());
+    }
+    @Test
+    public void ifOneItemIsPushed_ThenReturnSize1(){
+        ringBuffer.push(0);
+        assertEquals(1, ringBuffer.size());
+
+    }
+
 }
