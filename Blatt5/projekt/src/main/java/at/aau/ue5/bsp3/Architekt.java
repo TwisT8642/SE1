d4298e189df306f93f0a3396c2e3ee0f06729e08
TwisT8642 <baisan.andreas@gmail.com>
2022-01-23 12:23:36 +0100

    SE1_5_3_Refactoring

---
+++ Blatt5/projekt/src/main/java/at/aau/ue5/bsp3/Architekt.java
@@ -0,0 +1,23 @@
+package at.aau.ue5.bsp3;
+
+public class Architekt extends Person {
+    private Feld feld;
+
+    public Architekt(String vorname, String nachname, Integer alter, Feld feld) {
+        super(vorname, nachname, alter);
+        this.feld = feld;
+    }
+
+    public Feld getFeld() {
+        return feld;
+    }
+
+    public void setFeld(Feld feld) {
+        this.feld = feld;
+    }
+
+    @Override
+    public String getJobBeschreibung() {
+        return "Designed die Architektur des Projekts";
+    }
+}
